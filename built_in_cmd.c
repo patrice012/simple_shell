d80ad5d5410dee2a07ce9ea6f1ac8a6348299290
patrice02 <patricedev02@gmail.com>
2023-05-18 17:56:39 +0000

    add env build-in command

---
+++ built_in_cmd.c
@@ -0,0 +1,39 @@
+#include "header.h"
+
+/**
+ * shell_exit - Implement the exit built-in, that exits the shell
+ * Return: None
+ */
+
+/*
+ * Usage: exit
+ * Exit the shell with a status code of 0 (success)
+ */
+
+void shell_exit(void)
+{
+    exit(0);
+}
+
+/**
+ * shell_env - display the current environment variables in a shell.
+ * Return: None
+ */
+
+/*
+ * Usage: env
+ */
+
+
+void shell_env(void)
+{
+    char **env = environ;
+    int i = 0;
+
+    while (environ[i] != NULL)
+    {
+        print_str(environ[i]);
+        print_str("\n");
+        i++;
+    }
+}
--- shell.c
+++ shell.c
@@ -1,6 +1,6 @@
 #include "header.h"
 
-void shell_prompt(void);
+
 
 /**
  * main - main function
