390a672c51fe892fc2866f77468b102dc6d88850
patrice02 <patricedev02@gmail.com>
2023-05-18 19:02:13 +0000

    add clean function

--- helper.c
+++ helper.c
@@ -22,4 +22,11 @@ void build_absolute_path(char *directory, char *filename, char *absolute_path)
  * Return: 0 if success and -1 if fail
  */
 
+/* this function should take a variadic parameters and free all pointers */
+
+void clean_up(char *ptr, ...)
+{
+  /* code */
+}
+
 
